4c4e3e1b9d9ae84d1d3c3b1b5e2851d316b27af5
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-04-10 23:19:46 +0000

    feat(aula3): finalizando aula3
    criando state e inserindo projetos

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
+import { key, store } from './store'
 import '@fortawesome/fontawesome-free/css/all.css'
 import router from './router'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).use(store, key).mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,30 @@
-import { createStore } from 'vuex'
+import Projeto from '@/interfaces/Projeto'
+import { InjectionKey } from 'vue'
+import { createStore, Store } from 'vuex'
 
-export default createStore({
-  state: {},
+interface state {
+  projetos: Projeto[]
+}
+
+export const key: InjectionKey<Store<state>> = Symbol()
+
+export const store = createStore<state>({
+  state: {
+    projetos: [
+      {
+        id: new Date().toISOString(),
+        nome: 'Typescript'
+      },
+      {
+        id: new Date().toISOString(),
+        nome: 'Vue'
+      },
+      {
+        id: new Date().toISOString(),
+        nome: 'Vuex'
+      }
+    ]
+  },
   getters: {},
   mutations: {},
   actions: {},
