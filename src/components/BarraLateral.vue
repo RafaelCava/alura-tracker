6a12bf663c52152fc1956d090ab2097c223d6497
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-04-10 23:03:19 +0000

    feat(aula2): finalizando aula2
    na barra lateral foi inserido uma nav para ir para projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,7 +1,25 @@
 <template>
   <header>
-    <h1><img src="@/assets/logo.png" alt="logo" /></h1>
-    <button class="button" @click="alteraTema">{{ textoDoBotao }}</button>
+    <div class="has-text-centered">
+      <h1><img src="@/assets/logo.png" alt="logo" /></h1>
+      <button class="button" @click="alteraTema">{{ textoDoBotao }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link class="link" :to="{ name: 'Tarefas' }">
+            <i class="fas fa-tasks" />
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link class="link" :to="{ name: 'Projetos' }">
+            <i class="fas fa-project-diagram" />
+            Projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -40,7 +58,16 @@ header
   background: #0d3b66
   width: 100%
   height: 100vh
-  text-align: center
+
+.panel
+  li
+    margin: 8px 0
+.link
+  color: #fff
+  &:hover
+    color: #faf0ca
+  &.router-link-active
+    color: #faf0ca
 
 @media only screen and (max-width: 768px)
   header
